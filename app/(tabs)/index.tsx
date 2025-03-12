049254878c7a695108ab40e5807b46e167679d89
Nichita <N00233189@iadt.ie>
2025-03-12 01:20:25 +0000

    Add Events screen and sample event data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,37 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import Feather from '@expo/vector-icons/Feather';
 
-import { ScreenContent } from '~/components/ScreenContent';
 
-export default function Home() {
+export default function Events() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Events' }} />
+        <Stack.Screen options={{ title: 'Events' }} />
+
+        {/* Event list item container */}
+        <View className='gap-3 p-3'>
+            <View className='flex-row'>
+                <View className='flex-1'>
+                    <Text className='text-lg font-semibold text-gray-500 uppercase'>Wed 11 Sep · 19:30 CET</Text>
+                    <Text className='text-xl font-bold'>This is the title</Text>
+                    <Text className='text-gray-700'>eCommerce in Barcelona</Text>
+                </View>
+                {/* event image */}
+                <Image 
+                source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/9.jpg'}}
+                className='w-2/5 aspect-video'
+                />
+            </View>
+
+            {/* Footer */}
+            <View className='flex-row gap-3'>
+                <Text className='text-gray-600 mr-auto'>16 Going · ICON BCN</Text>
+
+                <Feather name="bookmark" size={24} color="gray" />
+                <Feather name="share" size={24} color="gray" />
+            </View>
+        </View>
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
