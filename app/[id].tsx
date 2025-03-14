549248a82e61099a70b4263d284d7d79be061e95
Nichita <N00233189@iadt.ie>
2025-03-14 00:46:02 +0000

    event detail page conted added + Styled.

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,5 +1,6 @@
-import { useLocalSearchParams } from 'expo-router';
-import { View, Text, StyleSheet } from 'react-native';
+import { useLocalSearchParams, Stack } from 'expo-router';
+import { View, Text, Image } from 'react-native';
+import dayjs from "dayjs";
 
 import events from "../assets/events.json"
 
@@ -8,5 +9,28 @@ export default function EventPage() {
 
     const event = events.find((e) => e.id === id)
 
-    return <Text>Event name: {event?.title}</Text>
+    if (!event) {
+        return <Text>Event not found</Text>
+    }
+    
+    return (
+
+        <View className='flex-1 p-3 gap-3 bg-white'>
+            <Stack.Screen options={{ title: "Event", headerBackTitleVisible: false, headerTintColor: "black"}}/>
+            
+            <Image 
+                source={{uri: event.image}}
+                className='w-full aspect-video rounded-lg'
+            />
+            <Text className='text-4xl font-bold' numberOfLines={2}>{event.title}</Text>
+            <Text className='text-lg font-semibold text-gray-500 uppercase'>{dayjs(event.datetime).format("ddd, D, MMM")} · {dayjs(event.datetime).format("h:mm A")}</Text>
+            <Text className='text-lg'>{event.description}</Text>
+        </View>    
+    
+    
+    
+    
+    
+    )
+        
 }
